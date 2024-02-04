30b61f207c246e6ecb070fe50062db4c270a4797
CAUCO70 <156064242+CAUCORASEKO@users.noreply.github.com>
2024-02-04 23:46:23 +0200

    pieniä muutoksia

--- kysely/views.py
+++ kysely/views.py
@@ -1,7 +1,24 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from .models import Kysymys
 
 
 def indeksi(request):
-    return HttpResponse("Heippa! Olet kysely-appin idex-sivulla.")
+    kysymyslista = Kysymys.objects.order_by("-julkaisupvm")[:2]
+    context = {
+        "kysymykset": kysymyslista,
+    }
+    return render(request, "kysely/indeksi.html", context)
+
+
+def näytä(request, question_id):
+    return HttpResponse(f"Katsot juuri kysymystä {question_id}")
+
+
+def tulokset(request, question_id):
+    return HttpResponse(f"Katsot kysymyksen {question_id} tuloksia")
+
+
+def äänestä(request, question_id):
+    return HttpResponse(f"Olet äänestämässä kysymykseen {question_id}")
